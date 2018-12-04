d71b08992df84adec1c4915d952be66f83450fea
Brice LEPORINI <bleporini@palo-it.com>
2018-12-04 17:07:12 +0100

    Adding unit tests

--- 05_11_2018_Taxes/src/main/java/com/paloit/kata/model/Facture.java
+++ 05_11_2018_Taxes/src/main/java/com/paloit/kata/model/Facture.java
@@ -1,36 +1,1 @@
-package com.paloit.kata.model;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Facture {
-
-	private List<LigneFacture> ligneFactures;
-	private Float pttc;
-	private Float montantTaxe;
-
-	public List<LigneFacture> getLigneFactures() {
-		return ligneFactures;
-	}
-
-	public void setLigneFactures(List<LigneFacture> ligneFactures) {
-		this.ligneFactures = ligneFactures;
-	}
-
-	public Float getPttc() {
-		return pttc;
-	}
-
-	public void setPttc(float pttc) {
-		this.pttc = pttc;
-	}
-
-	public Float getMontantTaxe() {
-		return montantTaxe;
-	}
-
-	public void setMontantTaxe(float montantTaxe) {
-		this.montantTaxe = montantTaxe;
-	}
-
-}
+package com.paloit.kata.model;import java.util.ArrayList;import java.util.List;public class Facture {	private List<LigneFacture> ligneFactures = new ArrayList<>();	private Float pttc = 0f;	private Float montantTaxe = 0f;	public List<LigneFacture> getLigneFactures() {		return ligneFactures;	}	public void setLigneFactures(List<LigneFacture> ligneFactures) {		this.ligneFactures = ligneFactures;	}	public Float getPttc() {		return pttc;	}	public void setPttc(float pttc) {		this.pttc = pttc;	}	public Float getMontantTaxe() {		return montantTaxe;	}	public void setMontantTaxe(float montantTaxe) {		this.montantTaxe = montantTaxe;	}}
--- 05_11_2018_Taxes/src/main/java/com/paloit/kata/model/LigneFacture.java
+++ 05_11_2018_Taxes/src/main/java/com/paloit/kata/model/LigneFacture.java
@@ -1,50 +1,1 @@
-package com.paloit.kata.model;
-
-public class LigneFacture {
-
-	private Produit produit;
-	private Integer quantite;
-	private Float prixHT;
-	private Float prixTTC;
-	
-
-	public LigneFacture(Produit produit, int quantite) {
-		this.produit = produit;
-		this.quantite = quantite;
-
-	}
-
-	public Produit getProduit() {
-		return produit;
-	}
-
-	public void setProduit(Produit produit) {
-		this.produit = produit;
-	}
-
-	public Integer getQuantite() {
-		return quantite;
-	}
-
-	public void setQuantite(int quantite) {
-		this.quantite = quantite;
-	}
-	
-
-	public Float getPrixHT() {
-		return prixHT;
-	}
-
-	public void setPrixHT(float prixHT) {
-		this.prixHT = prixHT;
-	}
-
-	public Float getPrixTTC() {
-		return prixTTC;
-	}
-
-	public void setPrixTTC(float prixTTC) {
-		this.prixTTC = prixTTC;
-	}
-
-}
+package com.paloit.kata.model;public class LigneFacture {	private Produit produit;	private Integer quantite;	private Float prixHT = 0f;	private Float prixTTC = 0f;		public LigneFacture(Produit produit, int quantite) {		this.produit = produit;		this.quantite = quantite;	}	public Produit getProduit() {		return produit;	}	public void setProduit(Produit produit) {		this.produit = produit;	}	public Integer getQuantite() {		return quantite;	}	public void setQuantite(int quantite) {		this.quantite = quantite;	}		public Float getPrixHT() {		return prixHT;	}	public void setPrixHT(float prixHT) {		this.prixHT = prixHT;	}	public Float getPrixTTC() {		return prixTTC;	}	public void setPrixTTC(float prixTTC) {		this.prixTTC = prixTTC;	}}
